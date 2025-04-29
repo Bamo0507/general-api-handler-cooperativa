a807414f8231218089bfcdf166f750ed101892e2
Pedro Avila <pedroavila@Pedros-Laptop.local>
2025-04-28 20:36:30 -0600

    Fixing generics

--- src/endpoints/graphql_endpoints.rs
+++ src/endpoints/graphql_endpoints.rs
@@ -1,7 +1,13 @@
 use actix_web::{web, HttpResponse};
-use juniper::http::{graphiql::graphiql_source, GraphQLRequest};
+use juniper::{
+    http::{graphiql::graphiql_source, GraphQLRequest},
+    GraphQLType, GraphQLTypeAsync,
+};
 
-use super::handlers::graphql::payment::{create_payment_schema, PaymentContext, PaymentSchema};
+use super::handlers::{
+    configs::schema_configs::GeneralSchema,
+    graphql::payment::{create_payment_schema, PaymentContext, PaymentQuery},
+};
 
 //This is pretty much boilerplate for any Graphql api
 
@@ -9,7 +15,10 @@ pub fn graphql_config(config: &mut web::ServiceConfig) {
     let payment_schema = create_payment_schema();
     config
         .app_data(payment_schema)
-        .service(web::resource("/graphql").route(web::post().to(graphql)))
+        .service(
+            web::resource("/graphql/Payment")
+                .route(web::post().to(graphql::<PaymentQuery, PaymentContext>)),
+        )
         .service(web::resource("/graphiql").route(web::get().to(graphiql)));
 }
 
@@ -22,13 +31,27 @@ async fn graphiql() -> HttpResponse {
         .body(html);
 }
 
+//Fucking hate the dependecy on llms
 //TODO: Make this to accept Generic Schemas
-async fn graphql(
+async fn graphql<GenericQuery, GenericContext>(
     data: web::Json<GraphQLRequest>,
-    schema: web::Data<PaymentSchema>,
-) -> HttpResponse {
-    let context = PaymentContext {};
+    schema: web::Data<GeneralSchema<GenericQuery, GenericContext>>,
+    context: GenericContext,
+) -> HttpResponse
+//TODO: comment all this typing later
+where
+    GenericQuery: GraphQLTypeAsync<Context = GenericContext>
+        + GraphQLType<Context = GenericContext>
+        + Send
+        + Sync
+        + 'static,
+    GenericQuery::Context: Send + Sync,
+    GenericQuery::TypeInfo: Send + Sync,
+{
     let res = data.execute(&schema, &context).await;
 
     return HttpResponse::Ok().json(res);
 }
+
+//  expected reference `&<T as GraphQLValue>::Context`
+//      found reference `&PaymentContext`
