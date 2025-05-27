b90463abcd1a852ac9e1c8810d3793cf42ae8bc5
PeDro0210 <pedroruben0210@gmail.com>
2025-05-27 01:10:43 -0600

    adding setup for env config

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
+mod config;
 mod endpoints;
 mod models;
 mod repos;
 
 use actix_cors::Cors;
 use actix_web::{App, HttpServer};
+use config::Env;
 use endpoints::graphql_endpoints::graphql_config;
-//use config::Env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    //* For production
-    // let config = Env::env_init();
+    // !For production
+    let config = Env::env_init();
 
-    // let port = config.port;
-    // let host = config.host;
-
-    //* For dev
-    let port = 5050;
-    let host = "0.0.0.0";
+    let port = config.port;
+    let host = config.host;
 
     std::env::set_var("RUST_LOG", "debug");
     env_logger::init();
