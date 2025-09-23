48d5f809d50e4b9fceb48d2bb0b5f67867e50dbe
PeDro0210 <pedroruben021@gmail.com>
2025-09-22 22:37:46 -0600

    needed-for-feature/chore: added get_multiple_models in to payments

--- src/repos/graphql/payment.rs
+++ src/repos/graphql/payment.rs
@@ -9,7 +9,7 @@ use crate::{
         graphql::{Affiliate, Payment, PaymentHistory},
         redis::Payment as RedisPayment,
     },
-    repos::auth::utils::hashing_composite_key,
+    repos::{auth::utils::hashing_composite_key, graphql::utils::get_multiple_models},
 };
 pub struct PaymentRepo {
     pub pool: web::Data<Pool<Client>>,
@@ -43,55 +43,11 @@ impl PaymentRepo {
     }
 
     pub fn get_user_payments(&self, access_token: String) -> Result<Vec<Payment>, String> {
-        let mut con = self.pool.get().expect("Couldn't connect to pool");
-
-        let db_access_token = hashing_composite_key(&[&access_token]);
-
-        match con.scan_match::<String, String>(format!("users:{}:payments:*", db_access_token)) {
-            Ok(keys) => {
-                let mut payment_list: Vec<Payment> = Vec::new();
-
-                // conn for fetching payments
-                let mut con = self.pool.get().expect("Couldn't connect to pool");
-
-                for key in keys {
-                    // We first fetch the raw data, first
-                    let user_payment_raw = con
-                        .json_get::<String, &str, RedisValue>(key.to_owned(), "$")
-                        .unwrap(); // I will do it in one line, but nu uh, it would be unreadable
-
-                    // for some reason redis gives all the info deserialize, so I have to do the
-                    // serializion process my self
-                    let nested_data = from_redis_value::<String>(&user_payment_raw).unwrap(); // first is
-
-                    // ik that I could've made the direct mapping to the GraphQl object, but I
-                    // rather using my own name standar for the redis keys and that Bryan manages
-                    // the names as however he want's it
-                    let user_payment_redis =
-                        from_str::<Vec<RedisPayment>>(nested_data.as_str()).unwrap()[0].clone();
-                    // cause
-                    // of the way  of the way the json library works on redis, the objects follow a
-                    // list type fetching, but as the db was planned, we where heading for a more
-                    // key aproach overall, so that's why we need the cast (after all there will
-                    // always be just one element)
-
-                    // now we do the payment mapping
-
-                    //payment_list.push(Payment {
-                    //    payment_id: get_payment_key(key),
-                    //    total_amount: user_payment_redis.quantity,
-                    //    payment_date: user_payment_redis.date_created,
-                    //    ticket_num: user_payment_redis.ticket_number,
-                    //    commentary: user_payment_redis.comments,
-                    //    photo: user_payment_redis.comprobante_bucket,
-                    //    state: user_payment_redis.status,
-                    //});
-                }
-
-                Ok(payment_list)
-            }
-            Err(_) => Err("Couldn't get users payments".to_string()),
-        }
+        get_multiple_models::<Payment, RedisPayment>(
+            access_token,
+            self.pool.clone(),
+            "payments".to_owned(), // TODO: see a way to don't burn the keys
+        )
     }
 
     //TODO: refactor for the affiliate_key jus to be a simple array
