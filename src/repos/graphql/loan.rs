504fbeccfca5a7350eec493eb1ce05f8b6725441
PeDro0210 <pedroruben021@gmail.com>
2025-09-22 22:42:24 -0600

    needed-for-feature/chore: added get_multiple_models in to loan

--- src/repos/graphql/loan.rs
+++ src/repos/graphql/loan.rs
@@ -6,6 +6,7 @@ use redis::{from_redis_value, Commands, JsonCommands, Value as RedisValue};
 use regex::Regex;
 use serde_json::from_str;
 
+use crate::repos::graphql::utils::get_multiple_models;
 use crate::{
     models::{
         graphql::{Codeudor, Loan, Pagare, PrestamoDetalles},
@@ -26,47 +27,11 @@ impl LoanRepo {
 
     //TODO: implent true logic
     pub fn get_user_loans(&self, access_token: String) -> Result<Vec<Loan>, String> {
-        let mut con = self.pool.get().expect("Couldn't connect to pool");
-
-        let db_access_token = hashing_composite_key(&[&access_token]);
-
-        match con.scan_match::<String, String>(format!("users:{}:loans:*", db_access_token)) {
-            Ok(keys) => {
-                let mut loans_list: Vec<Loan> = Vec::new();
-
-                // conn for fetching payments
-                let mut con = self.pool.get().expect("Couldn't connect to pool");
-
-                for key in keys {
-                    // We first fetch the raw data, first
-                    let user_loan_raw = con
-                        .json_get::<String, &str, RedisValue>(key.to_owned(), "$")
-                        .unwrap(); // I will do it in one line, but nu uh, it would be unreadable
-
-                    println!("{user_loan_raw:?}");
-                    // for some reason redis gives all the info deserialize, so I have to do the
-                    // serializion process my self
-                    let nested_data = from_redis_value::<String>(&user_loan_raw).unwrap(); // first is
-                                                                                           // just the path, second is the actual data
-
-                    // ik that I could've made the direct mapping to the GraphQl object, but I
-                    // rather using my own name standar for the redis keys and that Bryan manages
-                    // the names as however he want's it
-                    let user_loan_redis =
-                        from_str::<Vec<RedisLoan>>(nested_data.as_str()).unwrap()[0].clone();
-                    // cause
-                    // of the way  of the way the json library works on redis, the objects follow a
-                    // list type fetching, but as the db was planned, we where heading for a more
-                    // key aproach overall, so that's why we need the cast (after all there will
-                    // always be just one element)
-
-                    // now we do the loan mapping
-                }
-
-                Ok(loans_list)
-            }
-            Err(_) => Err("Couldn't get users payments".to_string()),
-        }
+        get_multiple_models::<Loan, RedisLoan>(
+            access_token,
+            self.pool.clone(),
+            "loans".to_owned(), // TODO: see a way to don't burn the keys
+        )
     }
 
     //TODO: add later
