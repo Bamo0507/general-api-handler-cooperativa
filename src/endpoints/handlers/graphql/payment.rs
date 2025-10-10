71cf36c9ee4207f83eb1ad6403a19af2581c524f
Diego <131807665+dlop6@users.noreply.github.com>
2025-10-09 19:19:21 -0600

    feat: mutation para aprobar o rechazar pago

--- src/endpoints/handlers/graphql/payment.rs
+++ src/endpoints/handlers/graphql/payment.rs
@@ -62,4 +62,14 @@ impl PaymentMutation {
             account_number,
         )
     }
+
+    /// Mutation para aprobar o rechazar un pago
+    pub async fn approve_or_reject_payment(
+        context: &GeneralContext,
+        id: String,
+        new_state: String,
+        commentary: String,
+    ) -> Result<Payment, String> {
+        context.payment_repo().approve_or_reject_payment(id, new_state, commentary).await
+    }
 }
