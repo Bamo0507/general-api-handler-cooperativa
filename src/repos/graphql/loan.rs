2fd2b6881d48f538eaceba63f567f8b643d56ced
Diego <131807665+dlop6@users.noreply.github.com>
2025-10-30 21:57:24 -0600

    feat: creat loan implementado, falta tesitng

--- src/repos/graphql/loan.rs
+++ src/repos/graphql/loan.rs
@@ -5,7 +5,7 @@ use redis::Client;
 use redis::{from_redis_value, Commands, JsonCommands, Value as RedisValue};
 use serde_json::from_str;
 
-use crate::repos::graphql::utils::get_multiple_models_by_id;
+use crate::repos::graphql::utils::{get_db_access_token_with_affiliate_key, get_multiple_models_by_id};
 use crate::{
     models::{graphql::Loan, redis::Loan as RedisLoan},
     repos::auth::utils::hashing_composite_key,
@@ -28,6 +28,52 @@ impl LoanRepo {
             self.pool.clone(),
             "loans".to_owned(), // TODO: see a way to don't burn the keys
         )
+    }
+
+    pub fn create_loan(
+        &self,
+        affiliate_key: String,
+        total_quota: i32,
+        base_needed_payment: f64,
+        reason: String,
+    ) -> Result<String, String> {
+        let mut con = &mut self.pool.get().expect("Couldn't connect to pool");
+
+        // obtenemos el db_access_token desde el affiliate_key
+
+        let db_access_token =
+            get_db_access_token_with_affiliate_key(affiliate_key, self.pool.clone())?;
+
+        if let Ok(keys) =
+            con.scan_match::<String, String>(format!("users:{}:loans:*", db_access_token))
+        {
+            let keys_parsed: Vec<String> = keys.collect();
+
+            // para crear el loan y evitar colisiones
+            let loan_hash_key =
+                hashing_composite_key(&[&keys_parsed.len().to_string(), &db_access_token]);
+
+            let con = &mut self.pool.get().expect("Couldn't connect to pool");
+
+            let _: () = con
+                .json_set(
+                    format!("users:{}:loans:{}", db_access_token, loan_hash_key),
+                    "$",
+                    &RedisLoan {
+                        total_quota,
+                        base_needed_payment,
+                        payed: 0.0,
+                        debt: base_needed_payment,
+                        total: base_needed_payment,
+                        status: "PENDING".to_owned(),
+                        reason,
+                    },
+                )
+                .expect("LOAN CREATION: Couldn't Create Loan");
+            return Ok("Loan Created".to_owned());
+        }
+
+        Err("LOAN CREATION: Couldn't Create Loan".to_owned())
     }
 
     //pub fn add_ill_pay(&self, loan_id: String, ill_pay: Pagare) -> () {}
